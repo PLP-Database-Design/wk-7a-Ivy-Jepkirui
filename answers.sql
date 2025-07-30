e686bcba31587fd4dfee2e94b7cf1ee1a031fc65
IvyJepkirui <101108464+Ivy-Jepkirui@users.noreply.github.com>
2025-07-30 14:44:48 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,32 +1,49 @@
 /*SQL query to transform this table into 1NF, ensuring that each row represents a single product for an order*/
 
-SELECT 
-    OrderID,
-    CustomerName,
-    TRIM(product) AS Product
-FROM 
-    ProductDetail,
-    UNNEST(STRING_TO_ARRAY(Products, ',')) AS product;
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
+);
+
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
 
 
 /*SQL query to transform this table into 2NF by removing partial dependencies. Ensure that each non-key column fully depends on the entire primary key.
 step one*/
 
-CREATE TABLE Orders AS
-SELECT DISTINCT 
-    OrderID,
-    CustomerName
-FROM 
-    OrderDetails;
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
 
-/*step 2*/
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
 
-CREATE TABLE OrderItems AS
-SELECT 
-    OrderID,
-    Product,
-    Quantity
-FROM 
-    OrderDetails;
+CREATE TABLE Product (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
 
+INSERT INTO Product (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
 
